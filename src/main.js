6172095e2cceb91a6e3e814b1a5489d3bf418b62
MeperDonas <santienrike16anime@gmail.com>
2025-03-25 18:06:47 -0500

    actualizacion del inicio y se añadieron las vistas
    Se dejo el inicio con las opciones del convertidor y se añadieron las vistas del convertidorPDF, UrltoPdf y historial de archivos convertidos

--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
 import "./css/home.css";
 
+// Navegación
 document.getElementById("convertFile").addEventListener("click", () => {
-  window.location.href = "/views/convertPdf.html"; // Redirige a la vista de conversión de archivos
+  window.location.href = "/views/convertPdf.html";
 });
 
 document.getElementById("convertLink").addEventListener("click", () => {
-  window.location.href = ""; // Redirige a la vista de conversión de links
+  window.location.href = "/views/convertUrltoPdf.html";
 });
 
 document.getElementById("history").addEventListener("click", () => {
-  window.location.href = ""; // Redirige a la vista del historial
+  window.location.href = "/views/historyConvert.html";
 });
+
+// Manejo de selección de archivos
+const fileInput = document.getElementById("fileInput");
+if (fileInput) {
+  fileInput.addEventListener("change", (event) => {
+    const files = event.target.files; // Obtiene la lista de archivos
+    console.log(files); // Muestra los archivos en la consola para verificar
+
+    for (let file of files) {
+      console.log(`Archivo seleccionado: ${file.name}`);
+    }
+  });
+}
